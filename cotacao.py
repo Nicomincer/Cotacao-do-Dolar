aa06d5520a83f61dfc54e6e67422f7ade03e9f55
Vésper <nicolasalencar100@gmail.com>
2023-02-19 21:50:41 -0300

    versão 1.0

---
+++ cotacao.py
@@ -0,0 +1,24 @@
+import urllib.request
+import tkinter 
+
+def pegarcotação():
+    try:
+        req = urllib.request.urlopen("https://dolarhoje.com/")
+        texto = req.read().decode("utf8")
+        valor=texto[texto.find('id="nacional"')+21:texto.find('id="nacional"')+25]
+        return valor 
+
+    except:
+        print("Erro. Verifique sua conexão com a internet.")
+
+programa = tkinter.Tk()
+programa.title("Pegar cotação do dolar")
+valordodolar = tkinter.Label(programa, text='', font=('Arial', 50))
+valordodolar.pack()
+valordodolar.configure(text=pegarcotação())
+
+programa.mainloop()
+
+
+
+
